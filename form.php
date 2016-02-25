dd6d27e62d4a86d088d87306d16cd32cf227324c
benimoler <fischerbeni@gmail.com>
2016-02-25 02:11:35 +0100

    Version1.0
    first upload to GitHub

---
+++ form.php
@@ -0,0 +1,14 @@
+<?php
+
+defined('C5_EXECUTE') or die(_("Access Denied."));
+
+?>
+<div>
+    <label>Titel:</label>
+    <input type="text" class="form-control" name="title" value="<?php echo $title?>">
+</div>
+<br>
+<div>
+    <label>Text:</label>
+    <input type="text" class="form-control" name="text" value="<?php echo $text?>">
+</div>
---
+++ view.php
@@ -0,0 +1,5 @@
+<?php defined('C5_EXECUTE') or die(_("Access Denied.")) ?>
+
+<h2><?php echo htmlspecialchars($title); ?></h2>
+<?php echo htmlspecialchars($text); ?>
+
